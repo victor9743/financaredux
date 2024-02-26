eb28e7c5ae487722d701728fa59496c6faf34ce0
victor costa <victorcosta9743@gmail.com>
2024-02-26 16:20:53 -0300

    feature

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="row container">
+      <div className="card card-primary container col-md-3">
+        <div className="card-title">
+          <label>sdafasdf</label>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
