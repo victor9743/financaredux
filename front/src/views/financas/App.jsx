a8014e5ed0ce473643829598a0e3369a6c52edc8
Victor Costa <70709626+victor9743@users.noreply.github.com>
2024-02-28 21:01:27 -0300

    feature

--- front/src/views/financas/App.jsx
+++ front/src/views/financas/App.jsx
@@ -5,6 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const financas = useSelector((state) => state.produto.value);
+  const format_financa = (valor) => {
+    return valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+  }
 
   const entrada = () => {
     let entrada = 0;
@@ -14,15 +17,32 @@ function App() {
       }
     })
 
-    console.log(entrada);
     return entrada;
   }
+
+  const saida = () => {
+    let saida = 0;
+    financas.forEach((financa) => {
+      if (financa.tipo_entrada === 2) {
+        saida += financa.valor;
+      }
+    })
+
+    return saida;
+  }
+
+  const saldo = () => {
+
+    return entrada() - saida();
+
+  }
+  
   return (
     <div className="container">
         <div className="row d-flex justify-content-between">
-          <CardFinanca bg_card="bg-success" text_card="text-light" card_titulo="Receita" card_valor={entrada()} />
-          <CardFinanca bg_card="bg-danger" text_card="text-light" card_titulo="Despesa" card_valor="0,00" />
-          <CardFinanca bg_card="bg-primary" text_card="text-light" card_titulo="Saldo" card_valor="0,00" />
+          <CardFinanca bg_card="bg-success" text_card="text-light" card_titulo="Receita" card_valor={`+ ${format_financa(entrada())}`} />
+          <CardFinanca bg_card="bg-danger" text_card="text-light" card_titulo="Despesa" card_valor={`- ${format_financa(saida())}`} />
+          <CardFinanca bg_card="bg-primary" text_card="text-light" card_titulo="Saldo" card_valor={`${entrada() > saida() ? '+' : '-'} ${format_financa(saldo())}`} />
         </div>
         <div className="table table-responsive">
           <Table />
--- front/src/views/financas/New.jsx
+++ front/src/views/financas/New.jsx
@@ -1,11 +1,56 @@
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { salvar } from '../../slices/produtosSlice';
 
 export const New = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [financa, setFinanca] = useState({});
+
+    const handleFinanca = (event) => {
+        let valor = event.target.value;
+
+        if (event.target.name === "valor") {
+            const rawValue = event.target.value.replace(/[^0-9]/g, '');
+    
+            let formattedValue = '';
+            if (rawValue.length > 2) {
+                formattedValue = `${rawValue.slice(0, -2)},${rawValue.slice(-2)}`;
+            } else if (rawValue.length === 2) {
+                formattedValue = `0,${rawValue}`;
+            } else if (rawValue.length === 1) {
+                formattedValue = `0,0${rawValue}`;
+            }
+
+            const valorNumerico = parseFloat(formattedValue.replace(/[^\d,]/g, '').replace(',', '.'));
+    
+            // Verificar se o valor é um número válido
+            if (isNaN(valorNumerico)) {
+                return '';
+            }
+        
+            // Formatar o valor como um valor monetário
+            event.target.value = valorNumerico.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            });
+
+            valor = format_valor(event.target.value);
+        }
+        setFinanca({...financa, [event.target.name]: valor })
+    }
+
+    const format_valor  = (valor) => {
+        valor = valor.replace(",", ".");
+        valor = valor.replace("R$", "");
+        return parseFloat(valor);
+    }
 
     const salvar = () => {
-        navigate("/");
+        dispatch(salvar(financa));
+        // navigate("/");
     }
 
     return (
@@ -16,19 +61,30 @@ export const New = () => {
                 </div>
                 <div className='mb-3 col-md-3'>
                     <label>Descrição</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" 
+                        className="form-control"
+                        name="descricao"
+                        onChange={(e) => handleFinanca(e)}
+                    />
                 </div>
                 <div className='mb-3 col-md-3'>
                     <label>Tipo de entrada</label>
-                    <select className='form-control'>
-                        <option>Selecione...</option>
-                        <option>Receita</option>
-                        <option>Despesa</option>
+                    <select className='form-control' 
+                        name='tipo_entrada' 
+                        onChange={(e) => handleFinanca(e)}
+                    >
+                        <option value="">Selecione...</option>
+                        <option value="1">Receita</option>
+                        <option value="2">Despesa</option>
                     </select>
                 </div>
                 <div className='mb-3 col-md-3'>
                     <label>Valor</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" 
+                        className="form-control" 
+                        name='valor'
+                        onChange={(e) => handleFinanca(e)}
+                    />
                 </div>
                 <div className="col-md-3 mt-4">
                     <Button className="btn btn-success" onClick={salvar}>Salvar</Button>
